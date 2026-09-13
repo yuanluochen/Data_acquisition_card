8b92f818e249f7be33ba5a566cb9fb15c95b7753
yuanluochen <yuanluochen@foxmail.com>
2026-09-13 17:46:50 +0800

    应用 .gitignore，移除已忽略的追踪文件

--- Slave_computer/STM32_DABM_D223/MDK-ARM/DABM-D223/clock.d
+++
@@ -1,9 +0,0 @@
-dabm-d223/clock.o: \
-  D:\Users\admin\AppData\Local\Arm\Packs\RealThread\RT-Thread\3.1.5\src\clock.c \
-  D:\Users\admin\AppData\Local\Arm\Packs\RealThread\RT-Thread\3.1.5\include\rthw.h \
-  D:\Users\admin\AppData\Local\Arm\Packs\RealThread\RT-Thread\3.1.5\include\rtthread.h \
-  RTE\RTOS\rtconfig.h \
-  D:\Users\admin\AppData\Local\Arm\Packs\RealThread\RT-Thread\3.1.5\include\rtdebug.h \
-  D:\Users\admin\AppData\Local\Arm\Packs\RealThread\RT-Thread\3.1.5\include\rtdef.h \
-  D:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdarg.h \
-  D:\Users\admin\AppData\Local\Arm\Packs\RealThread\RT-Thread\3.1.5\include\rtservice.h
--- Slave_computer/STM32_DABM_D223/MDK-ARM/DABM-D223/thread.d
+++
@@ -1,9 +0,0 @@
-dabm-d223/thread.o: \
-  D:\Users\admin\AppData\Local\Arm\Packs\RealThread\RT-Thread\3.1.5\src\thread.c \
-  D:\Users\admin\AppData\Local\Arm\Packs\RealThread\RT-Thread\3.1.5\include\rthw.h \
-  D:\Users\admin\AppData\Local\Arm\Packs\RealThread\RT-Thread\3.1.5\include\rtthread.h \
-  RTE\RTOS\rtconfig.h \
-  D:\Users\admin\AppData\Local\Arm\Packs\RealThread\RT-Thread\3.1.5\include\rtdebug.h \
-  D:\Users\admin\AppData\Local\Arm\Packs\RealThread\RT-Thread\3.1.5\include\rtdef.h \
-  D:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdarg.h \
-  D:\Users\admin\AppData\Local\Arm\Packs\RealThread\RT-Thread\3.1.5\include\rtservice.h
